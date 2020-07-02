5b010bb6d0f21cc8b8f50490b380d4f467cc861a
OlivierNDO <Olivier_N@lynchburg.edu>
2020-07-02 13:41:13 -0500

    file path configuration

---
+++ configuration/file_path_configuration.R
@@ -0,0 +1,5 @@
+### File Paths
+######################################################################################################
+config_fp_raw_data_dir = 'D:/insurance_data/'
+config_fp_raw_train_file = 'train.csv'
+config_fp_raw_train_file = 'test.csv'
